aa99822ebc14c8163057ff6fb44d96384b9dcd18
JoeKanauss <33582866+JoeKanauss@users.noreply.github.com>
2018-01-28 10:30:54 -0600

    wdv351 coursework

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="../topBarStyle.css">
+<style>
+#threeFiveOne{
+	background-color: black;
+	color: #a964e5;
+	font-weight: bold;
+	border: none;
+}
+</style>
+</head>
+<body>
+<?php include '../topBar.php';?>
+<div class="contain">
+<h1 class="titlePurple">WDV 351: Website Application Components</h1>
+<h2><a href="googleAnalytics.html">Google Analytics</a></h2>
+<h2><a href="socialMediaLinks.html">Social Media Links</a></h2>
+<h2><a href="youTubeVideoReview.html">YouTube Video Review</a></h2>
+<h2><a href="videoPlayers.html">Video Player Demonstrations</a></h2>
+<h2><a href="payPalDonate.html">PayPal Donate</a></h2>
+<h2><a href="payPalBuyNow.html">PayPal Buy Now</a></h2>
+<h2><a href="payPalSubscribe.html">PayPal Subscribe</a></h2>
+<h2><a href="payPalAddToCart.html">PayPal Add to Cart</a></h2>
+<h2><a href="payPalExpressCheckout.html">PayPal Express Checkout</a></h2>
+<h2><a href="payPalDigitalGoods.html">PayPal Digital Goods</a></h2>
+<h2><a href="dataVisualization.html">Data Visualization</a></h2>
+
+</div>
+
+</body>
+</html>
